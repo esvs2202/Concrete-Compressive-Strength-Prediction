0210e95b114cd61590f08b9bfa71fac2f88a049f
Sriram E.S.V <sriramesv96@gmail.com>
2021-09-26 00:12:32 +0530

    Update request.py

--- request.py
+++ request.py
@@ -6,7 +6,8 @@ r = requests.post(url, json={'Age (in days) :': 28,
                              'Cement (in kg) : ': 540.0,
                              'Water (in kg) :': 162.0,
                              'Fly ash (in kg) :': 0.0,
+                             'Super plasticizer (in kg) :': 2.5
                              'Blast furnace slag (in kg) ': 0.0,
-                             'Super plasticizer (in kg) :': 2.5})
+                             })
 
 print(r.json())
