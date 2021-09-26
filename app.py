5b8939290ad490da05674c64856876abc22167cb
Sriram E.S.V <sriramesv96@gmail.com>
2021-09-26 20:36:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import joblib
 import numpy as np
 import pandas as pd
 from flask import Flask, render_template, request
-import logging
+# import logging
 
 """Application logging"""
 
-logging.basicConfig(filename='deployment_logs.log', level=logging.INFO,
-                    format='%(levelname)s:%(asctime)s:%(message)s')  # configuring logging operations
+# logging.basicConfig(filename='deployment_logs.log', level=logging.INFO,
+#                     format='%(levelname)s:%(asctime)s:%(message)s')  # configuring logging operations
 
 app = Flask(__name__)
 
@@ -32,9 +32,9 @@ def predict():
                   request.form.get('sp'), request.form.get('bfs')]  # list of inputs
 
         # logging operation
-        logging.info(f"Age (in days): {f_list[0]}, Cement (in kg): {f_list[1]},"
-                     f"Water (in kg): {f_list[2]}, Fly ash (in kg): {f_list[3]},"
-                     f"Superplasticizer (in kg): {f_list[4]}, Blast furnace slag (in kg): {f_list[5]}")
+#         logging.info(f"Age (in days): {f_list[0]}, Cement (in kg): {f_list[1]},"
+#                      f"Water (in kg): {f_list[2]}, Fly ash (in kg): {f_list[3]},"
+#                      f"Superplasticizer (in kg): {f_list[4]}, Blast furnace slag (in kg): {f_list[5]}")
 
         final_features = np.array(f_list).reshape(-1, 6)
         df = pd.DataFrame(final_features)
@@ -43,9 +43,9 @@ def predict():
         result = "%.2f" % round(prediction[0], 2)
 
         # logging operation
-        logging.info(f"The Predicted Concrete Compressive strength is {result} MPa")
+#         logging.info(f"The Predicted Concrete Compressive strength is {result} MPa")
 
-        logging.info("Prediction getting posted to the web page.")
+#         logging.info("Prediction getting posted to the web page.")
         return render_template('index.html',
                                prediction_text=f"The Concrete compressive strength is {result} MPa")
 
